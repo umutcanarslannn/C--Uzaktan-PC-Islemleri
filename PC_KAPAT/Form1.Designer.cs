482b67f2f947f97282cf26a3e2b3fb1a4f51c5ee
umutcanarslannn <umutcanarslan_711@hotmail.com>
2022-04-09 14:34:20 +0300

    C# PC Islemleri
    C# ve SQL ile uzaktan pc açıp/kapatma işlemleri.

---
+++ PC_KAPAT/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace PC_KAPAT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Kapat = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Aç = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.BackgroundImage = global::PC_KAPAT.Properties.Resources._5a059a909cf05203c4b6045b;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(275, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tüm Bilgisayarları Kapat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(500, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LAB B2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(224, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bilgisayar Listesi";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Kapat});
+            this.dataGridView1.Location = new System.Drawing.Point(46, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(474, 275);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Kapat
+            // 
+            this.Kapat.HeaderText = "Kapat";
+            this.Kapat.Name = "Kapat";
+            this.Kapat.Text = "Kapat";
+            this.Kapat.ToolTipText = "Kapat";
+            this.Kapat.UseColumnTextForButtonValue = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(612, 129);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1, 279);
+            this.panel1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(46, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(202, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Yenile";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(360, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.Location = new System.Drawing.Point(275, 462);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(245, 30);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Sıfırla";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(228, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(124, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Aç});
+            this.dataGridView2.Location = new System.Drawing.Point(685, 129);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(490, 275);
+            this.dataGridView2.TabIndex = 9;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(907, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Bilgisayar Listesi";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(685, 416);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(490, 40);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Tüm Bilgisayarları Aç";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Aç
+            // 
+            this.Aç.HeaderText = "Aç";
+            this.Aç.Name = "Aç";
+            this.Aç.Text = "Aç";
+            this.Aç.ToolTipText = "Aç";
+            this.Aç.UseColumnTextForButtonValue = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(548, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Uzaktan Kontrol";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1211, 501);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewButtonColumn Kapat;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewButtonColumn Aç;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PC_KAPAT/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Net;
+using System.Threading;
+
+namespace PC_KAPAT
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlCommand cmd,cmd2;
+        SqlConnection con;
+        SqlCommand komut,komut2;
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Server=192.168.0.2;Database= B2;User Id=veritabani;Password=vt123;");
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "update kapat set kapat2=@kapat";
+            cmd.Parameters.AddWithValue("@kapat", 1);
+            cmd.ExecuteNonQuery();
+        //  Thread.Sleep(3000);
+            
+        }
+
+        void pclistesi()
+        {
+          
+            con = new SqlConnection("Server=192.168.0.2;Database=B2;User Id=veritabani;Password=vt123");
+            da = new SqlDataAdapter("Select pc_ad from pc_kapat", con);
+            con.Open();
+            da.Fill(ds);
+            con.Close();
+            dataGridView1.DataSource = ds.Tables[0];
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            acilacakpclistesi();
+            button3.Visible = false;
+            pclistesi();
+            dataGridView2.AllowUserToAddRows = false;
+            timer1.Enabled = true;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Server=192.168.0.2; Database=B2;User Id=veritabani;Password=vt123;");
+           komut.Connection = con;           
+            con.Open(); 
+            string bilgisayaradi = Dns.GetHostName();
+            komut.CommandText = "insert into pc_kapat (pc_ad) values (@pc_ad)";
+            komut.Parameters.AddWithValue("@pc_ad", bilgisayaradi.ToString());
+
+            komut.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex==0)
+            {               
+             
+                if (MessageBox.Show("Kapatmak istiyormusunuz","Uyarı",MessageBoxButtons.YesNo)==DialogResult.Yes)
+                {
+                    con = new SqlConnection("Server=192.168.0.2;Database=B2;User Id=veritabani;Password=vt123");
+                    komut = new SqlCommand();
+                    //komut2 = new SqlCommand();
+                    komut.Connection = con;
+                    con.Open();
+                    komut.CommandText = "update pc_kapat set durum=@durum where pc_ad='" + dataGridView1.CurrentRow.Cells[1].Value.ToString() + "'";
+                    komut.Parameters.AddWithValue("@durum", 1);
+                    komut.ExecuteNonQuery();
+                    con.Close();           
+                }
+            }
+        }
+
+
+        public static bool WakeOnLan(string MacAddress)
+        {
+            try
+            {
+                MacAddress = MacAddress.Replace("-", "");
+                MacAddress = MacAddress.Replace(":", "");
+                MacAddress = MacAddress.Replace(".", "");
+                MacAddress = MacAddress.Replace("_", "");
+                MacAddress = MacAddress.Replace(" ", "");
+                MacAddress = MacAddress.Replace(",", "");
+                MacAddress = MacAddress.Replace(";", "");
+                if (MacAddress.Length != 12)
+                {
+                    //return ;  
+                }
+
+                byte[] mac = new byte[6];
+
+
+                for (int k = 0; k < 6; k++)
+                {
+                    mac[k] = Byte.Parse(MacAddress.Substring(k * 2, 2), System.Globalization.NumberStyles.HexNumber);
+                }
+
+                System.Net.Sockets.UdpClient client = new System.Net.Sockets.UdpClient();
+                client.Connect(System.Net.IPAddress.Broadcast, 5004);
+
+
+                byte[] packet = new byte[17 * 6];
+
+                for (int i = 0; i < 6; i++)
+                    packet[i] = 0xFF;
+
+                for (int i = 1; i <= 16; i++)
+                    for (int j = 0; j < 6; j++)
+                        packet[i * 6 + j] = mac[j];
+
+                client.Send(packet, packet.Length);
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("HATA");
+                return false;
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           // ds.Tables.Clear();
+           // pclistesi();
+      
+           
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            ds.Tables.Clear();
+            pclistesi();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*komut = new SqlCommand();
+            con = new SqlConnection("Server=192.168.0.2; Database=B2;User Id=veritabani;Password=vt123;");
+            komut.Connection = con;
+            komut.CommandText = "Delete from pc_kapat where pc_ad='" + Dns.GetHostName().ToString() + "'";
+            con.Open();
+            komut.ExecuteNonQuery();
+            con.Close();*/
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Server=192.168.0.2; Database=B2;User Id=veritabani;Password=vt123;");
+            con.Open();
+            cmd2 = new SqlCommand();
+            cmd2.Connection = con;
+            cmd2.CommandText = "update kapat set kapat2=@kapat";
+            cmd2.Parameters.AddWithValue("@kapat", 0);
+            cmd2.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Kapatmak istiyormusunuz", "Uyarı", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                con = new SqlConnection("Server=192.168.0.2;Database=B2;User Id=veritabani;Password=vt123");
+                komut = new SqlCommand();
+                //komut2 = new SqlCommand();
+                komut.Connection = con;
+                con.Open();
+                
+                komut.CommandText = "update pc_kapat set durum=@durum where pc_ad='" + dataGridView1.CurrentRow.Cells[1].Value.ToString() + "'";
+                komut.Parameters.AddWithValue("@durum", 1);
+                komut.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex==0)
+            {
+                WakeOnLan(dataGridView2.CurrentRow.Cells[2].Value.ToString());
+            }
+        }
+        
+        void acilacakpclistesi()
+        {
+            
+            con = new SqlConnection("Server=192.168.0.2;Database=B2;User Id=veritabani;Password=vt123");
+            da = new SqlDataAdapter("Select Pc_ad,adres from mac_adresleri", con);
+            con.Open();
+            da.Fill(ds2);
+            con.Close();
+            dataGridView2.DataSource = ds2.Tables[0];
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int a = dataGridView2.RowCount;
+            for(int i =0 ;i<a;i++)
+            {
+                WakeOnLan(dataGridView2.Rows[i].Cells[2].Value.ToString());
+            }
+        }
+    }
+}
